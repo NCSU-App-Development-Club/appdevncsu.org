4b4952c731ab290e8e2e21ca68220ff0419b268c
Abel Lu <alu4@ncsu.edu>
2025-05-06 18:03:33 -0400

    remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./background.css";
 import App from "./App.tsx";
 
-createRoot(document.querySelector("body")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.querySelector("body")!).render(<App />);
