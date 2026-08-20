71b8aa348d7c90ad778c28852b94889631b5378f
ex4dev <31103658+ex4dev@users.noreply.github.com>
2026-08-19 22:47:01 -0400

    Add social links to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,25 @@
+import linkedinLogo from '../../assets/images/linkedin.svg'
+import instagramLogo from '../../assets/images/instagram.svg'
+import discordLogo from '../../assets/images/discord.svg'
+
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#222] min-h-24 text-center flex items-center justify-center z-40 relative">
-      © 2025 NC State App Development Club
+    <footer className="w-full bg-[#222] min-h-24 text-center flex flex-col items-center justify-center z-40 relative gap-2">
+      © 2026 NC State App Development Club
+      <div className="flex gap-1 items-center">
+        <a
+          href="https://www.linkedin.com/company/ncsu-app-development-club"
+          target="_blank"
+        >
+          <img src={linkedinLogo} className="w-8" />
+        </a>
+        <a href="https://www.instagram.com/appdevncsu/" target="_blank">
+          <img src={instagramLogo} className="w-8" />
+        </a>
+        <a href="https://discord.gg/2XVpkUgSfG" target="_blank">
+          <img src={discordLogo} className="w-8" />
+        </a>
+      </div>
     </footer>
   )
 }
